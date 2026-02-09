f1b2e045c375d57ddbd159141a4b4a311a553da5
Yuntae Jin <jyjin3@me.com>
2026-02-09 19:35:27 +0900

    init basis

---
+++ clearml-client/main.py
@@ -0,0 +1,3 @@
+from clearml import Task
+
+task = Task.init(project_name="my project", task_name="my task")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from clearml-poc!")
+
+
+if __name__ == "__main__":
+    main()
